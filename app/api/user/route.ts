c19848216ef05fa017701d373ccefda010f2eacf
Ankur Semle <semleankur@gmail.com>
2025-02-05 23:20:36 +0530

    add prisma and add postgres

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-  return Response.json({ username: "Ankur Semle", email: "ankur@gmail.com" });
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+
+  return Response.json({
+    message: "Your are logged in!",
+  });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import axios from "axios";
+import { Signup } from "@/components/Signup";
 
-async function getUserDetails() {
-  await new Promise((r) => setTimeout(r, 2000));
-  const response = await axios.get("http://localhost:3000/api/user");
-  return response.data;
-}
-
-export default async function Home() {
-  const userData = await getUserDetails();
-
-  return (
-    <div className="flex flex-col justify-center h-screen">
-      <div className="flex justify-center">
-        <div className="border p-8 rounded">
-          <div>Name: {userData?.username}</div>
-
-          {userData?.email}
-        </div>
-      </div>
-    </div>
-  );
+export default function () {
+  return <Signup />;
 }
